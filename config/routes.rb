553feea55d8325284517ae94bb3229ce560c76b3
Xiaochen Chen <xiaochenchen.git@gmail.com>
2014-05-11 19:34:22 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
   #get "static_pages/home"
   root 'static_pages#home'
 
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
+  match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
