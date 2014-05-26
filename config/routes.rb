28eb73e7ca912556a83c1f666a31c890e383a8e0
Xiaochen Chen <xiaochenchen.git@gmail.com>
2014-05-26 00:30:45 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   #get "static_pages/home"
   root 'static_pages#home'
@@ -9,6 +10,8 @@ SampleApp::Application.routes.draw do
   #get "static_pages/about"
   #get "static_pages/contact"
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
